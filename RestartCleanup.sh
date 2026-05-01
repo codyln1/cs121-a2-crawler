d8674b7dc3a5a1b1e33d296d3c88bcc88c25fac3
Caden Lee <cadenl4@uci.edu>
2026-05-01 10:14:02 -0700

    chore: restart script

---
+++ RestartCleanup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Clean up the file logs for restarting the crawler. Run from project root
+rm frontier.shelve
+rm Logs/Worker.log
+rm Logs/report_hashes.txt
+rm Logs/report_longest_page.txt
+rm Logs/report_word_frequencies.txt
+ls
+ls Logs
